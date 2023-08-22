2bf4f79fd0914859ba6871c07805baeb17d64890
Nazar Pinonzhyk <nz2develop@gmail.com>
2023-08-22 16:33:32 +0300

    Add ability to jump onto elevations

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,12 +7,14 @@ using UnityEngine.AI;
 public class Player : MonoBehaviour {
     
     [SerializeField] private LayerMask groundMask;
+    [SerializeField] private LayerMask solidObjectsMask;
     [SerializeField] private float speed = 3f;
     [SerializeField] private float jumpMaxHight = 1f;
     [SerializeField] private float jumpDuration = 1f;
     [SerializeField] private AnimationCurve jumpCurve;
 
     private NavMeshAgent navMeshAgent;
+    private bool flying;
     private float jumpStartTime;
     private float jumpStartY;
 
@@ -38,16 +41,39 @@ public class Player : MonoBehaviour {
         }
 
         if (Input.GetKeyDown(KeyCode.Space)) {
-            jumpStartTime = Time.time;
-            jumpStartY = transform.position.y;
+            StartJump();
         }
 
-        if (jumpStartTime + jumpDuration > Time.time) {
-            var jumpTime = Time.time - jumpStartTime;
-            var jumpProgress = jumpTime / jumpDuration;
-            var jumpHightDelta = jumpCurve.Evaluate(jumpProgress);
-            transform.position = new Vector3(transform.position.x, jumpStartY + jumpHightDelta, transform.position.z);
+        if (flying) {
+            if (jumpStartTime + jumpDuration > Time.time) {
+                var jumpTime = Time.time - jumpStartTime;
+                var jumpProgress = jumpTime / jumpDuration;
+                var jumpHightDelta = jumpCurve.Evaluate(jumpProgress) * jumpMaxHight;
+                var desiredPosition = new Vector3(transform.position.x, jumpStartY + jumpHightDelta, transform.position.z);
+                transform.position = desiredPosition;
+                if (jumpProgress > 0.5f && Physics.CheckSphere(desiredPosition, 0.2f, solidObjectsMask)) {
+                    GroundJump();
+                }
+            } else {
+                GroundJump();
+            }
         }
     }
 
+    private void StartJump() {
+        flying = true;
+        navMeshAgent.updatePosition = false;
+        jumpStartTime = Time.time;
+        jumpStartY = transform.position.y;
+    }
+
+    private void GroundJump() {
+        flying = false;
+        if (NavMesh.SamplePosition(transform.position, out var hit, 2f, NavMesh.AllAreas)) {
+            navMeshAgent.Warp(hit.position);
+        } else {
+            navMeshAgent.Warp(transform.position);
+        }
+        navMeshAgent.updatePosition = true;
+    }
 }
