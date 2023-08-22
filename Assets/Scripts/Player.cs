dc7369e3123a46597bfa458182b7663038309e43
Nazar Pinonzhyk <nz2develop@gmail.com>
2023-08-22 13:22:35 +0300

    Implement player jump

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,8 +7,13 @@ public class Player : MonoBehaviour {
     
     [SerializeField] private LayerMask groundMask;
     [SerializeField] private float speed = 3f;
+    [SerializeField] private float jumpMaxHight = 1f;
+    [SerializeField] private float jumpDuration = 1f;
+    [SerializeField] private AnimationCurve jumpCurve;
 
     private NavMeshAgent navMeshAgent;
+    private float jumpStartTime;
+    private float jumpStartY;
 
     private void Awake() {
         navMeshAgent = GetComponent<NavMeshAgent>();
@@ -30,6 +35,18 @@ public class Player : MonoBehaviour {
             if (Physics.Raycast(mouseRay, out var hit, groundMask)) {
                 navMeshAgent.SetDestination(hit.point);
             }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            jumpStartTime = Time.time;
+            jumpStartY = transform.position.y;
+        }
+
+        if (jumpStartTime + jumpDuration > Time.time) {
+            var jumpTime = Time.time - jumpStartTime;
+            var jumpProgress = jumpTime / jumpDuration;
+            var jumpHightDelta = jumpCurve.Evaluate(jumpProgress);
+            transform.position = new Vector3(transform.position.x, jumpStartY + jumpHightDelta, transform.position.z);
         }
     }
 
