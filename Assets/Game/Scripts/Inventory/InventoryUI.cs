7fd291816e511b8c52f2a251a5fc9792f24ab7ea
Nazar Pinonzhyk <nz2develop@gmail.com>
2023-09-21 17:07:51 +0300

    Implement new inventory layout and open/close animation

--- Assets/Game/Scripts/Inventory/InventoryUI.cs
+++ Assets/Game/Scripts/Inventory/InventoryUI.cs
@@ -6,9 +6,18 @@ public class InventoryUI : MonoBehaviour {
     
     [SerializeField] private Inventory inventory;
     [SerializeField] private GameObject inventoryRoot;
+    [SerializeField] private RectTransform mask;
+    [SerializeField] private RectTransform container;
+    [SerializeField] private float changeDuration = 0.5f;
+
+    private bool changing;
+    private float changeStartTime;
+    private Vector2 changeSizeDelta;
+    private Vector2 lastSizeDelta;
 
     private void Awake() {
         inventory.OnOpenRequest += Open;
+        inventoryRoot.SetActive(true);
     }
 
     private void Start() {
@@ -16,11 +25,31 @@ public class InventoryUI : MonoBehaviour {
     }
 
     public void Open() {
-        inventoryRoot.SetActive(true);
+        ChangeState(true);
+    }
+
+    private void ChangeState(bool open) {
+        changing = true;
+        changeStartTime = Time.time;
+        lastSizeDelta = mask.sizeDelta;
+        changeSizeDelta = open ? container.sizeDelta : new Vector2(0, container.sizeDelta.y);
+    }
+
+    private void Update() {
+        if (changing) {
+            var changeEndTime = changeStartTime + changeDuration;
+            if (Time.time < changeEndTime) {
+                var changeProgress = (Time.time - changeStartTime) / changeDuration;
+                mask.sizeDelta = Vector2.Lerp(lastSizeDelta, changeSizeDelta, changeProgress);
+            } else {
+                changing = false;
+                mask.sizeDelta = changeSizeDelta;
+            }
+        }
     }
 
     public void Close() {
-        inventoryRoot.SetActive(false);
+        ChangeState(false);
     }
 
 }
