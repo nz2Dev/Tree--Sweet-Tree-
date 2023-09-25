59f13af64ab4f7c4fbbb05792890d88d144547af
Nazar Pinonzhyk <nz2develop@gmail.com>
2023-09-25 12:14:27 +0300

    Add possibility to have multiple set of audios

--- Assets/Game/Scripts/Character/HovanetsAudio.cs
+++ Assets/Game/Scripts/Character/HovanetsAudio.cs
@@ -1,26 +1,33 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
+[Serializable]
+public struct ClipsSet {
+    public AudioClip[] clips;
+}
 
 public class HovanetsAudio : MonoBehaviour {
     
     [SerializeField] private AudioSource leftFootSource;
     [SerializeField] private AudioSource rightFootSource;
 
-    [SerializeField] private AudioClip[] walkClips;
+    [SerializeField] private ClipsSet[] walkClipSets;
+    [SerializeField] private int selectedSetIndex;
 
     public void PlayLeftFoot() {
-        leftFootSource.clip = SelectRandom(walkClips);
+        leftFootSource.clip = SelectRandom(walkClipSets[selectedSetIndex].clips);
         leftFootSource.Play();
     }
 
     public void PlayRightFoot() {
-        rightFootSource.clip = SelectRandom(walkClips);
+        rightFootSource.clip = SelectRandom(walkClipSets[selectedSetIndex].clips);
         rightFootSource.Play();
     }
 
     private AudioClip SelectRandom(AudioClip[] clips) {
-        return clips[Random.Range(0, clips.Length)];
+        return clips[UnityEngine.Random.Range(0, clips.Length)];
     }
 
 }
