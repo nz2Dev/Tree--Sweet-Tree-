5e82759e35dc2dbf3937b1c365f905d326b2bef4
Nazar Pinonzhyk <nz2develop@gmail.com>
2023-09-29 10:52:44 +0300

    Fix fbx imports of models used for outline and preparations for builds

--- Assets/Game/Scripts/Utils/DistanceDebug.cs
+++ Assets/Game/Scripts/Utils/DistanceDebug.cs
@@ -6,11 +6,13 @@ using UnityEngine;
 public class DistanceDebug : MonoBehaviour {
      [SerializeField] private GameObject target;
 
+#if UNITY_EDITOR
      private void OnDrawGizmos() {
         Gizmos.color = Color.red;
         Gizmos.DrawLine(transform.position, target.transform.position);
         var distance = Vector3.Distance(target.transform.position, transform.position);
         Handles.Label(transform.position, "Distance: " + distance);
      }
+#endif
     
 }
