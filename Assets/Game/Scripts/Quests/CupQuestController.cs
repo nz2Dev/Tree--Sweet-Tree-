a8dec38ae6faf4a7af59143fa77cac2a4f39255d
Nazar Pinonzhyk <nz2develop@gmail.com>
2023-10-12 16:54:34 +0300

    Disable player character after camera cut

--- Assets/Game/Scripts/Quests/CupQuestController.cs
+++ Assets/Game/Scripts/Quests/CupQuestController.cs
@@ -9,22 +9,19 @@ public class CupQuestController : MonoBehaviour {
     [SerializeField] private CinemachineVirtualCamera questCamera;
     [SerializeField] private ActivationObject activationObject;
     [SerializeField] private Player player;
+    [SerializeField] private float cameraCutDuration = 0.9f;
 
     private bool activated;
+    private float activationStartTime;
 
     private void Awake() {
         activationObject.OnActivated += ActivationObjectOnActivated;
     }
 
     private void ActivationObjectOnActivated() {
+        activationStartTime = Time.time;
         questCamera.m_Priority += 2;
         activated = true;
-    }
-
-    public void OnCameraEndTransition() {
-        if (activated) {
-            player.GetComponentInChildren<Animator>().gameObject.SetActive(false);
-        }
     }
 
     private void OnDeactivate() {
@@ -35,6 +32,10 @@ public class CupQuestController : MonoBehaviour {
 
     private void Update() {
         if (activated) {
+            if (Time.time > activationStartTime + cameraCutDuration /*camera cut transition time*/ ) {
+                player.GetComponentInChildren<Animator>(true).gameObject.SetActive(false);
+            }
+
             if (Input.GetMouseButtonDown(1)) {
                 OnDeactivate();
             }
