07f096e922a71b1bd86bb19eb3a7dee0f414716c
Nazar Pinonzhyk <nz2develop@gmail.com>
2023-11-13 18:15:14 +0200

    Implement init exlusion state of transportable destination

--- Assets/Game/Scripts/Items/TransportableObjectDestination.cs
+++ Assets/Game/Scripts/Items/TransportableObjectDestination.cs
@@ -6,14 +6,22 @@ using UnityEngine.Events;
 
 public class TransportableObjectDestination : MonoBehaviour {
 
+    [SerializeField] private bool initIsExcluded;
+    [SerializeField] private UnityEvent<bool> OnContainObjectIsChangedEvent;
+
     private bool exludedFromActivation;
 
     public event Action<bool> OnContainObjectIsChanged;
 
     public bool IsExcludedFromActivation => exludedFromActivation;
 
+    private void Awake() {
+        SetIsExcluded(initIsExcluded);
+    }
+
     public void SetContainObject(bool containObject) {
         OnContainObjectIsChanged?.Invoke(containObject);
+        OnContainObjectIsChangedEvent?.Invoke(containObject);
     }
 
     public void SetIsExcluded(bool excluded) {
