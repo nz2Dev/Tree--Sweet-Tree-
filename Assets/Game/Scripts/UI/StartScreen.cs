81a18ab6c8d20b655706f8c9dbb7c82b958a265d
Nazar Pinonzhyk <nz2develop@gmail.com>
2023-12-12 17:48:32 +0200

    Add simple show/hide start screen UI

---
+++ Assets/Game/Scripts/UI/StartScreen.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class StartScreen : MonoBehaviour {
+    
+    [SerializeField] private GameObject root;
+    [SerializeField] private bool autoCompleteInEditor = false;
+    [SerializeField] private UnityEvent OnPlayActivated;
+
+    private void Start() {
+        if (Application.isEditor && autoCompleteInEditor) {
+            OnPlay();
+        }
+    }
+
+    public void OnPlay() {
+        root.SetActive(false);
+        OnPlayActivated?.Invoke();
+    }
+
+}
