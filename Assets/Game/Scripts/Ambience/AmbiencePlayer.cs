3cad10d7747e59db834736622182973227726f28
Nazar Pinonzhyk <nz2develop@gmail.com>
2023-12-13 16:10:52 +0200

    Add posibility to set and transition ambience volume

--- Assets/Game/Scripts/Ambience/AmbiencePlayer.cs
+++ Assets/Game/Scripts/Ambience/AmbiencePlayer.cs
@@ -6,14 +6,15 @@ public class AmbiencePlayer : MonoBehaviour {
 
     [SerializeField] private AudioSource audioSource;
     [SerializeField] private float cycleDelaySec = 30;
+    [SerializeField] private float initVolume = 0.03f;
 
     private bool isActive = true;
-    private float cachedVolumeLevel;
+    private float controledVolume;
 
     public bool IsActive => isActive;
 
     private void Awake() {
-        cachedVolumeLevel = audioSource.volume;
+        SetVolume(initVolume);
     }
 
     public void BeginAmbience() {
@@ -21,9 +22,40 @@ public class AmbiencePlayer : MonoBehaviour {
         StartCoroutine(SequenceRoutine());
     }
 
+    public void SetVolume(float volume) {
+        controledVolume = volume;
+        UpdateVolume();
+    }
+
+    private float transitionDuration;
+    private SequenceState transitionSequence;
+    private float startVolume;
+    private float targetVolume;
+
+    public void SetVolumeTransitionDuration(float duration) {
+        transitionDuration = duration;
+    }
+
+    public void SetVolumeTransition(float volume) {
+        transitionSequence = TweenUtils.StartSequence(transitionDuration);
+        startVolume = controledVolume;
+        targetVolume = volume;
+    }
+
+    private void UpdateVolumeTransition() {
+        if (TweenUtils.TryUpdateSequence(transitionSequence, out var progress)) {
+            SetVolume(Mathf.Lerp(startVolume, targetVolume, progress));
+        }
+        TweenUtils.TryFinishSequence(ref transitionSequence);
+    }
+
     public void SetIsActive(bool isActive) {
         this.isActive = isActive;
-        audioSource.volume = isActive ? cachedVolumeLevel : 0;
+        UpdateVolume();
+    }
+
+    private void UpdateVolume() {
+        audioSource.volume = isActive ? controledVolume : 0;
     }
 
     private IEnumerator SequenceRoutine() {
@@ -35,6 +67,8 @@ public class AmbiencePlayer : MonoBehaviour {
     }
 
     private void Update() {
+        UpdateVolumeTransition();
+
         if (Application.isEditor && Input.GetKeyDown(KeyCode.R)) {
             BeginAmbience();
         }
